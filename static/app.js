3073d57c7f1d781e81fa6e6113b190db15a7975e
adammcgrogan <adam.mcgrogan@dailypay.com>
2026-05-22 00:27:12 +0100

    feat: add Street / Satellite / Topo map style switcher

--- static/app.js
+++ static/app.js
@@ -10,10 +10,31 @@ const map = L.map('map', { zoomControl: false }).setView([51.505, -0.09], 13);
 
 L.control.zoom({ position: 'topright' }).addTo(map);
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/">CARTO</a>',
-    maxZoom: 19,
-}).addTo(map);
+const tileLayers = {
+    street: L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/">CARTO</a>',
+        maxZoom: 19,
+    }),
+    satellite: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+        maxZoom: 19,
+    }),
+    topo: L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+        maxZoom: 17,
+    }),
+};
+
+let activeLayer = tileLayers.street;
+activeLayer.addTo(map);
+
+document.querySelectorAll('input[name="mapstyle"]').forEach(radio => {
+    radio.addEventListener('change', () => {
+        map.removeLayer(activeLayer);
+        activeLayer = tileLayers[radio.value];
+        activeLayer.addTo(map);
+    });
+});
 
 let marker = null;
 let casingLayer = null;
